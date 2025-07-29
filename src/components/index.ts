c36e542dc69de68e2ef40e18fae5a491b2795d8b
Zaur_Magomedov <zaurmag411@gmail.com>
2025-07-29 11:25:14 +0300

    Fix: export svg-icon path

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as ZmButton } from './zm-button/zm-button.vue'
 export { default as ZmInput } from './zm-input/zm-input.vue'
-export { default as SvgIcon } from '@/shared/ui/svg-icon.vue'
+export { default as SvgIcon } from '../shared/ui/svg-icon.vue'
